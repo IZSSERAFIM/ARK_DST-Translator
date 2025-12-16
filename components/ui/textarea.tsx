efb41ae9a5f1a6cc5ff2692d51e79775c817d8fd
IZSSERAFIM <237422698@qq.com>
2025-12-16 20:43:04 +0800

    update Vertical Scrollbar style

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[160px] w-full rounded-2xl bg-white/5 px-4 py-3 text-sm text-slate-50 placeholder:text-slate-400 shadow-inner shadow-black/20 outline-none transition focus:ring-2 focus:ring-cyan-400 focus:ring-offset-0 border border-white/10",
+          "flex min-h-[160px] w-full rounded-2xl bg-white/5 px-4 py-3 text-sm text-slate-50 placeholder:text-slate-400 shadow-inner shadow-black/20 outline-none transition focus:ring-2 focus:ring-cyan-400 focus:ring-offset-0 border border-white/10 frosted-scrollbar",
           className
         )}
         ref={ref}
